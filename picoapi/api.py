0c1e9fb2bfaa6e66adddd60e6091ae26fc5a8389
Wojciech Kiwerski <wojciech.kiwerski@idealapps.cloud>
2021-10-23 12:52:21 +0000

    picoapi/api: Add OpenAPI tags, and check if service was registered

--- picoapi/api.py
+++ picoapi/api.py
@@ -31,7 +31,17 @@ def register_uservice():
         },
     }
 
-    requests.get(os.getenv("API_REGISTER_PATH"), json=uservice_definition)
+    # Check if service was registered
+    # if no register else raise logger info
+    existing_services = json.loads( requests.get(os.getenv("API_STATUS_PATH")).text )
+    if len(existing_services) == 0:
+        requests.get(os.getenv("API_REGISTER_PATH"), json=uservice_definition)
+        return
+    for service in existing_services:
+        if str(service["host"]) == str(uservice_definition["host"]) and str(service["port"]) == str(uservice_definition["port"]):
+            logger.info( "%s on http://%s:%s exist!" % ( uservice_definition["name"], uservice_definition["host"], uservice_definition["port"] ) )
+        else:    
+            requests.get(os.getenv("API_REGISTER_PATH"), json=uservice_definition)
 
 
 async def healthcheck():
@@ -54,6 +64,7 @@ class PicoAPI(FastAPI):
             x for x in os.getenv("API_CORS_ALLOW_HEADERS", "*").split()
         ],
         on_startup = None,
+        openapi_tag: str = os.getenv("API_OPENAPI_TAG", "default"),
         *args,
         **kwargs
     ) -> None:
@@ -73,17 +84,17 @@ class PicoAPI(FastAPI):
         self.is_master = is_master
         self.services = []
         self.healthchecks = {}
-        self.add_api_route(api_health_path, healthcheck)
 
         if self.is_master:
             # add service registration
-            self.add_api_route("/register", self.add_service)
-            self.add_api_route("/services/status", self.get_services_status)
-            self.add_api_route("/services/definition", self.get_services_openapi)
+            self.add_api_route(api_health_path, healthcheck, include_in_schema=False)
+            self.add_api_route("/register", self.add_service, tags=[openapi_tag])
+            self.add_api_route("/services/status", self.get_services_status, tags=[openapi_tag])
+            self.add_api_route("/services/definition", self.get_services_openapi, tags=[openapi_tag])
 
         else:
             # add the service registration event
-            self.on_startup = [ register_uservice() ]
+            self.on_startup = [self.add_api_route(api_health_path, healthcheck, include_in_schema=False), register_uservice()]
 
     async def get_services_status(self):
         return JSONResponse(
